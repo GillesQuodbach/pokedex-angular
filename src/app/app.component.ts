824cb512675bd94a0340e487006068c98478ad3e
Gilles <quodbach.gilles@gmail.com>
2024-02-18 12:44:21 +0100

    ajout pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, Pipe, PipeTransform } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { POKEMONS } from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
+import { DatePipe, NgFor, NgIf } from "@angular/common";
+import { BorderCardDirective } from "./border-card.directive";
+import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [
+    RouterOutlet,
+    NgIf,
+    NgFor,
+    BorderCardDirective,
+    DatePipe,
+    PokemonTypeColorPipe,
+  ],
+  templateUrl: "./app.component.html",
   styles: [],
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  title = "ng-pokemon-app";
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+
+    if (pokemon) {
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Votre pokémon n'existe pas`);
+      this.pokemonSelected = pokemon;
+    }
+  }
 }
